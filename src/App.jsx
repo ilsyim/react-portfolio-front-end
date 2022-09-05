049165d64a47913e9d3cdeb81695b89b0808e7ae
ilysim <illymarie1@live.com>
2022-09-05 14:48:37 -0500

    change switch color

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css"
 import Landing from './pages/Landing/Landing'
 import { useState } from 'react';
 import { createContext } from 'react'
+import ReactSwitch from "react-switch";
 export const ThemeContext = createContext(null)
 
 const App = () => {
@@ -15,6 +16,10 @@ const App = () => {
       <ThemeContext.Provider value={{theme, toggleTheme}}>
         <div className="App" id={theme}>
           <Landing />
+          <div className="switch">
+            <label>{theme === "light" ? "Light Mode" : "Dark Mode"}</label>
+            <ReactSwitch onChange={toggleTheme} checked={theme === "light"} onColor="#4377b0" onHandleColor="#000" offHandleColor="#4377b0" offColor="#000" width={60} height={26} handleDiameter={29}/>
+          </div>
         </div>
       </ThemeContext.Provider>
     </>
