8409035c03248b1bfb8ac42edf9f491363030a48
ilysim <illymarie1@live.com>
2022-09-22 16:01:30 -0500

    add picture of me to about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,12 +8,14 @@ const About = () => {
   return (
     <>
       <article className="article">
-        <h2>Welcome!</h2>
+        <h2>Howdy!</h2>
+
+        <a href="https://www.linkedin.com/in/ilyanasimmons/" target="_blank" rel="noopener noreferrer"><img src="Ilyana.JPG" alt="Me!" className="me" /></a>
           <p>
-            I am a software engineer currently based in Fort Worth, Texas. I graduated from UT with a BA in English.
+            I am a software engineer currently based in Fort Worth, Texas. I graduated from UT with a BA in English in May 2020 and graduated General Assembly's SEIR course in August 2022.
           </p>
           <p>
-            I joined the SEI course at General Assembly because I want my work to challenge how I view and solve problems.
+            I love being challenged and solving puzzles like sudoku, escape rooms, and brain teasers. 
           </p>
       </article>
     </>
