1abc73f2cf51f054ede9747dfe909db2c2e44ac8
ilysim <illymarie1@live.com>
2022-09-22 22:57:24 -0500

    add to about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -12,10 +12,10 @@ const About = () => {
 
         <a href="https://www.linkedin.com/in/ilyanasimmons/" target="_blank" rel="noopener noreferrer"><img src="Ilyana.JPG" alt="Me!" className="me" /></a>
           <p>
-            I am a software engineer currently based in Fort Worth, Texas. I graduated from UT with a BA in English in May 2020 and graduated General Assembly's SEIR course in August 2022.
+            I'm a software engineer currently based in Fort Worth, Texas. I love solving puzzles like sudoku and escape rooms. I wanted a challenge and in August 2022, I graduated from General Assembly's SEIR course.
           </p>
           <p>
-            I love being challenged and solving puzzles like sudoku, escape rooms, and brain teasers. 
+            When I'm not coding, I'm listening to <a href="https://open.spotify.com/playlist/0c2UI952YaXOvyzyFEI6uG?si=ea28f9f9dae048a1" target="_blank" rel="noopener noreferrer">music</a>, adding to my book collection, catching up on <a href="https://twitter.com/LFC" target="_blank" rel="noopener noreferrer">soccer</a>, or finally crossing a show off of my "to watch" list.
           </p>
       </article>
     </>
